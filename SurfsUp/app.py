24fd8a15459546ef6a889983fe93ebb4750c679f
mihle3124 <143448727+mihle3124@users.noreply.github.com>
2023-11-14 22:40:21 -0800

    updating climate_starter.ipynb file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,19 +1,28 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("sqlite:///SurfsUp/Resources/hawaii.sqlite")
+
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Base.classes.keys()
 
 # Create our session (link) from Python to the DB
 
